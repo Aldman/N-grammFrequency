a3832f604c950e3a8a3ca585e779d43a8bcb12f6
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-06-29 15:04:27 +0300

    Initialize commit

---
+++ SentencesParserTask.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentencesArray = text.Split(new char [] { '.', '!', '?', ';', ':', '(', ')' });
+            if (sentencesArray != null)
+                foreach (var sentence in sentencesArray)
+                    sentencesList.Add(SplitSentenceByWords(sentence));
+            return sentencesList;
+        }
+
+        private static List<string> SplitSentenceByWords(string sentence)
+        {
+            var wordBuilder = new StringBuilder();
+            var sentenceByWords = new List<string>();
+            if (!String.IsNullOrEmpty(sentence))
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (Char.IsLetter(sentence[i]) || sentence[i] == '\'')
+                    {
+                        wordBuilder.Append(sentence[i]);
+                        if (sentence.Length == i + 1)
+                        {
+                            sentenceByWords.Add(wordBuilder.ToString().ToLower());
+                            wordBuilder.Clear();
+                        }
+                    }
+                    else
+                    {
+                        if (wordBuilder.Length > 0)
+                            sentenceByWords.Add(wordBuilder.ToString().ToLower());
+                        wordBuilder.Clear();
+                    }
+                }
+            return sentenceByWords;
+        }
+
+        private static string GetLowerTextAndClearBuilder(StringBuilder wordBuilder)
+        {
+            string stringToReturn = wordBuilder.ToString().ToLower();
+            wordBuilder.Clear();
+            return stringToReturn;
+        }
+    }
+}
