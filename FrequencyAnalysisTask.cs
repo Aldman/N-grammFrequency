f0c2d3c0331c6bf33465f8c714d121e404009985
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-06-30 21:30:20 +0300

    First try by solve to FrequencyAnalysisTask

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TextAnalysis
 {
@@ -7,8 +8,33 @@ namespace TextAnalysis
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
-            //...
+            
             return result;
         }
-   }
+
+        private static void CheckNGrams (List<List<string>> text)
+        {
+            var countOfNgramms = new Dictionary<string, Dictionary<ValueTuple, int>>();
+            foreach (var sentence in text)
+            {
+                for (int i = 0; i < sentence.Count; i++)
+                {
+
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        private static Dictionary<string, string> ReturnBigrams(List<List<string>> text)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private static Dictionary<string, string> ReturnTrigrams(List<List<string>> text)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
 }
